5a76274b115b0a7283bd2b4f8b428b9de51085cd
Enzzo D.N. <enzzodiasnogueira2@gmail.com>
2024-05-06 14:58:47 -0300

    Update enviar_email.py

--- src/model/enviar_email.py
+++ src/model/enviar_email.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 import logging
 import os
 
-def enviar_email(email_sender, senha_sender):       
+def enviar_email(email_sender, senha_sender, email_receiver, parts, logo):       
     # ----- VARIAVEIS -----
     host = 'smtp.gmail.com'
     port = '587'
@@ -18,25 +18,36 @@ def enviar_email(email_sender, senha_sender):
 
     msg = MIMEMultipart()
     subject = 'BOT - PROJETO WEB SCRAPING'
-    body = 
+    body = f"""
+    <h2>{parts[0]}</h2> <img src = "cid:python_logo" alt = "Logo Python">
+    <p>{parts[1]}</p>
+    <p>{parts[2]}</p>
+    <p>{parts[3]}</p>
+    <p>{parts[4]}</p>
+    
+    Caso queira saber mais sobre esta linguagem de programação <a href="https://pt.wikipedia.org/wiki/Python">CLIQUE AQUI</a>!
+    """
 
     # ----- EMAIL -----
 
-    msg['From'] = usuario
-    msg['To'] = receiver
+    msg['From'] = email_sender
+    msg['To'] = email_receiver
     msg['Subject'] = subject
+
+    image = open('C:/logo_python.png', 'rb')
+    msg_image = MIMEImage(image.read())
+    image.close()
+
+    msg_image.attach('Content-ID', '<Logo Python>')
+    msg.attach(msg_image)
+
+    msg.attach(MIMEText(body, 'html'))
 
     # ----- ENVIO DO EMAIL -----
     logging.info('-- ENVIANDO EMAIL --')
-    server.sendmail(msg['From'], receiver.split(','), msg.as_string()) # PARA MAIS DE 1 EMAIL
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
         
-    print(f"-- EMAIL DA EMPRESA - {empresa} - {tipo} - ENVIADO COM SUCESSO PARA: {receiver} --\n")
+    print(f"-- EMAIL ENVIADO COM SUCESSO PARA: {email_receiver} --\n")
     server.quit()
 
-def apagar_prints(empresa):
-    for i in range(1,5):
-        if os.path.exists(f'C:/RPA/arquivos/printscreen_nps_byd_{empresa}_vendas_{i}.png'):
-            os.remove(f'C:/RPA/arquivos/printscreen_nps_byd_{empresa}_vendas_{i}.png')
-    for i in range(1,6):
-        if os.path.exists(f'C:/RPA/arquivos/printscreen_nps_byd_{empresa}_pos_vendas_{i}.png'):
-            os.remove(f'C:/RPA/arquivos/printscreen_nps_byd_{empresa}_pos_vendas_{i}.png')
+    os.remove('C:/logo_python.png')
