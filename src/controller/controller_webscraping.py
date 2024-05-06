03454e5cc1ae5df02cfe03ade32846743ad091e7
Enzzo D.N. <enzzodiasnogueira2@gmail.com>
2024-05-06 14:58:45 -0300

    Update controller_webscraping.py

--- src/controller/controller_webscraping.py
+++ src/controller/controller_webscraping.py
@@ -3,12 +3,13 @@ os.system('cls')
 from src.model.trechos import trechos
 from src.model.enviar_email import enviar_email
 
-# trechos()
 res = 'n'
 while res != 's':
-    email_sender = input('DIGITE SEU EMAIL CORRETAMENTE: ')
-    senha_sender = input('DIGITE SUA SENHA CORRETAMENTE: ')
+    email_sender = input('DIGITE SEU EMAIL, CORRETAMENTE: ')
+    senha_sender = input('DIGITE SUA SENHA, CORRETAMENTE: ')
+    email_receiver = input('DIGITE O EMAIL QUE VAI RECEBER A MENSAGEM, CORRETAMENTE: ')
     res = input('VOCÊ CONFIRMA OS DADOS ACIMA? (s/n): ').lower()
     os.system('cls')
 
-enviar_email(email_sender, senha_sender)
+parts, logo = trechos()
+enviar_email(email_sender, senha_sender, email_receiver, parts, logo)
