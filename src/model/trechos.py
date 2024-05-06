821a2934f54702dc00126c6dc47e6786cc90cf8c
Enzzo D.N. <enzzodiasnogueira2@gmail.com>
2024-05-06 14:58:48 -0300

    Update trechos.py

--- src/model/trechos.py
+++ src/model/trechos.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+parts = []
+
 def trechos():
     driver = webdriver.Chrome()
     driver.get('https://pt.wikipedia.org/wiki/Python')
@@ -12,10 +14,17 @@ def trechos():
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[3]/main/header/h1')))
     name = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[3]/main/header/h1')
     print(name.text)
+    parts.append(name.text)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/table[1]/tbody/tr[2]/th/small/span/a/img')))
+    logo = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/table[1]/tbody/tr[2]/th/small/span/a/img')
+    logo.screenshot('C:/logo_python.png')
 
     for i in range(4):
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[{i+1}]')))
         p = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[{i+1}]')
         print(f'\n{p.text}')
+        parts.append(p.text)
 
     driver.quit()
+    return parts, logo
